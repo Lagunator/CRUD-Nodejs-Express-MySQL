7c2d0186d1caaa1d1e2f4e62826e0d544be972fb
¨Lagunator¨ <jhosisasmauricio@gmail.com>
2024-11-17 22:02:40 -0400

    fix.env

--- src/db.js
+++ src/db.js
@@ -1,13 +1,15 @@
 import mysql from "mysql2/promise";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "", // Coloca tu contraseña de MySQL
-  database: "CrudNodejs",
+  host: process.env.DB_HOST || 'db', 
+  user: process.env.DB_USER || 'user',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'estudiantes',
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
+  queueLimit: 0
 });
 
 export default pool;
